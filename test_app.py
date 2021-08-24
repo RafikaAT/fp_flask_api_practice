157a43fb90e64ec907d99ea7fd36f24b57f72d72
Rafika <RTuremis@hotmail.co.uk>
2021-08-24 16:02:11 +0100

    Clean up test suite

--- test_app.py
+++ test_app.py
@@ -11,13 +11,7 @@ class TestAPICase():
         assert res.status == '200 OK'
         assert res.json[0]['name'] == 'Harry Potter and The Prisoner of Azkaban'
 
-    # def test_get_cat(self, api):
-    #     res = api.get('/api/cats/2')
-    #     assert res.status == '200 OK'
-    #     assert res.json['name'] == 'Test Cat 2'
-
     def test_get_movies_error(self, api):
         res = api.get('/dogs')
-        print(res.json)
-        assert res.status == '400 BAD REQUEST'
-        # assert "cat with id 4" in res.json['message']
+        # print(res.json)
+        assert res.status == '404 NOT FOUND'
